134591dae1ad670c1ec986e29d982af07b729942
Nurdaulet <b.nurdaulet.b@gmail.com>
2016-05-30 17:29:58 +0600

    Refactored

--- NSDate+Zodiac.swift
+++ NSDate+Zodiac.swift
@@ -6,66 +6,58 @@
 //  Copyright © 2016 Nurdaulet Bolatov. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 extension NSDate {
-  enum Zodiac: String {
-    case Ram = "ram_icon"
-    case Bull = "bull_icon"
-    case Twins = "twins_icon"
-    case Crab = "crab_icon"
-    case Lion = "lion_icon"
-    case Maiden = "maiden_icon"
-    case Scales = "scales_icon"
-    case Scorpion = "scorpion_icon"
-    case Archer = "archer_icon"
-    case MountainSeaGoat = "mountain_sea_goat_icon"
-    case WaterBearer = "water_bearer_icon"
-    case Fish = "fish_icon"
-    case None = ""
-  }
-  
-  func isBetweeenInclusive(date date1: NSDate, andDate date2: NSDate) -> Bool {
-    return date1.compare(self).rawValue * self.compare(date2).rawValue >= 0
-  }
-  
-  convenience init(dateString: String) {
-    let dateStringFormatter = NSDateFormatter()
-    dateStringFormatter.dateFormat = "yyyy-MM-dd"
-    let date = dateStringFormatter.dateFromString(dateString)!
-    self.init(timeInterval:0, sinceDate:date)
-  }
-  
-  func zodiac() -> Zodiac {
-    guard let gregorianCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian) else { return .None }
-    let dateComponents = gregorianCalendar.components([.Year], fromDate: self)
-    let year = dateComponents.year
-    if isBetweeenInclusive(date: NSDate(dateString: "\(year)-03-21"), andDate: NSDate(dateString: "\(year)-04-19")) {
-      return .Ram
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-04-20"), andDate: NSDate(dateString: "\(year)-05-20")) {
-      return .Bull
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-05-21"), andDate: NSDate(dateString: "\(year)-06-20")) {
-      return .Twins
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-06-21"), andDate: NSDate(dateString: "\(year)-07-22")) {
-      return .Crab
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-07-23"), andDate: NSDate(dateString: "\(year)-08-22")) {
-      return .Lion
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-08-23"), andDate: NSDate(dateString: "\(year)-09-22")) {
-      return .Maiden
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-09-23"), andDate: NSDate(dateString: "\(year)-10-22")) {
-      return .Scales
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-10-23"), andDate: NSDate(dateString: "\(year)-11-21")) {
-      return .Scorpion
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-11-22"), andDate: NSDate(dateString: "\(year)-12-21")) {
-      return .Archer
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-12-22"), andDate: NSDate(dateString: "\(year)-12-31"))
-      || isBetweeenInclusive(date: NSDate(dateString: "\(year)-01-01"), andDate: NSDate(dateString: "\(year)-01-19")){
-      return .MountainSeaGoat
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-01-20"), andDate: NSDate(dateString: "\(year)-02-18")) {
-      return .WaterBearer
-    } else if isBetweeenInclusive(date: NSDate(dateString: "\(year)-02-19"), andDate: NSDate(dateString: "\(year)-03-20")) {
-      return .Fish
+    enum Zodiac: String {
+        case Ram = "ram_icon"
+        case Bull = "bull_icon"
+        case Twins = "twins_icon"
+        case Crab = "crab_icon"
+        case Lion = "lion_icon"
+        case Maiden = "maiden_icon"
+        case Scales = "scales_icon"
+        case Scorpion = "scorpion_icon"
+        case Archer = "archer_icon"
+        case MountainSeaGoat = "mountain_sea_goat_icon"
+        case WaterBearer = "water_bearer_icon"
+        case Fish = "fish_icon"
     }
-    return .None
-  }
+    
+    func zodiac() -> Zodiac {
+        let gregorianCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        assert(gregorianCalendar != nil, "Gregorian calendar not initialized.")
+        let dateComponents = gregorianCalendar!.components([.Month, .Day], fromDate: self)
+        let month = dateComponents.month
+        let day = dateComponents.day
+        
+        switch (month, day) {
+        case (3, 21...31), (4, 1...19):
+            return .Ram
+        case (4, 20...30), (5, 1...20):
+            return .Bull
+        case (5, 21...31), (6, 1...20):
+            return .Twins
+        case (6, 21...30), (7, 1...22):
+            return .Crab
+        case (7, 23...31), (8, 1...22):
+            return .Lion
+        case (8, 23...31), (9, 1...22):
+            return .Maiden
+        case (9, 23...30), (10, 1...22):
+            return .Scales
+        case (10, 23...31), (11, 1...21):
+            return .Scorpion
+        case (11, 22...30), (12, 1...21):
+            return .Archer
+        case (12, 22...31), (1, 1...19):
+            return .MountainSeaGoat
+        case (1, 20...31), (2, 1...18):
+            return .WaterBearer
+        case (2, 19...29), (3, 1...20):
+            return .Fish
+        default:
+            assert(false, "Zodiac not found")
+        }
+    }
 }
